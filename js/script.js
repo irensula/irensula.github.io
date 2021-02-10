59ac27b03798f8b8adabe8a44a98d3c60d8803ef
irensula <irensula@gmail.com>
2021-02-10 10:31:55 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -63,35 +63,14 @@ $(document).ready(function () {
             }
         });
     });
-//GAME
-const car = document.getElementById("car");
-const lights = document.getElementById("lights");
 
-document.addEventListener("keydown", function(event) {
-    jump();
-});
-function jump() {
-     if (car.classList != "jump") {
-        car.classList.add("jump")
-    }
-    setTimeout(function() {
-        car.classList.remove("jump")
-    }, 300)
-}
-/*задержка анимации
-var el = $('.game img');
-$(window).scroll(function(){
-    if ( $(this).scrollTop() > el.offset().top - 400 ) {
-        el.addClass('lightsMov');
-    }
-});
+//DARK MODE
+/*if(!localStorage.theme) localStorage.theme = "light";
+    document.body.className = local.Storage.theme;
+    toggleThemeButton.innerText = document.body.classList.contains("dark") ? "Turn on the light" : "Turn off the light";
 */
-let isAlive = setInterval (function(){
-    let carTop = parseInt(window.getComputedStyle(car).getPropertyValue("top"));
-    let lightsLeft = parseInt(window.getComputedStyle(lights).getPropertyValue("left"));
-    
-    if (lightsLeft < 83 && lightsLeft > 17 && carTop >= 135) {
-        alert("GAME OVER!!")
-    }
-}, 20)
-
+toggleThemeButton.onclick = () => {
+    document.body.classList.toggle("dark");
+    toggleThemeButton.innerText = document.body.classList.contains("dark") ? "Turn on the light" : "Turn off the light";
+    localStorage.theme = document.body.className || "light";
+};
