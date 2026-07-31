37400481e7030ab0b120b5948b4a6fe068ddfbc7
irensula <irinawebdev@gmail.com>
2026-07-31 21:35:23 +0300

    update links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
                 <a href="https://github.com/irensula">
                     <i className="fa-brands fa-github"></i>
                 </a>
-                <a href="https://www.linkedin.com/in/irina-sula-022296129/">
+                <a href="https://www.linkedin.com/in/iryna-sula-022296129/">
                     <i className="fa-brands fa-linkedin"></i>
                 </a>
-                <a href="mailto:irensula19@example.com">
+                <a href="mailto:irensula19@gmail.com">
                     <i className="fa-solid fa-envelope"></i>
                 </a>
             </div>
